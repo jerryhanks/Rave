afd72d193bf33836aefd2bd8dba59637fd728cfe
Jerry Hanks Okafor <eobiwale@pacentltd.com>
2017-03-18 12:15:20 +0100

    refactor : rectored ravemodule

---
+++ rave/src/main/java/com/flutterwave/rave/Components/InputAmountWatcher.java
@@ -0,0 +1,27 @@
+package com.flutterwave.rave.Components;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by @Po10cio on 18/03/2017 for Rave.
+ */
+
+public class InputAmountWatcher implements TextWatcher {
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+}
