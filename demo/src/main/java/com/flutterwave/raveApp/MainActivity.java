c3ae174f2708662176ef6fc5e3d9830d94260d07
Jerry Hanks Okafor <eobiwale@pacentltd.com>
2017-03-23 16:21:13 +0100

    fix :  fixed tx nested in data object for card trasactions

--- demo/src/main/java/com/flutterwave/raveApp/MainActivity.java
+++ demo/src/main/java/com/flutterwave/raveApp/MainActivity.java
@@ -17,7 +17,10 @@ import flutterwave.com.raveApp.R;
 
 public class MainActivity extends AppCompatActivity implements RaveDialog.OnRaveResponseCallback {
 
-    private Button raveButton;
+    private Button raveButtonPin;
+    private Button raveButtonVbp;
+    private Button raveButtonNoAuth;
+    private Button raveButtonRandom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,71 +29,76 @@ public class MainActivity extends AppCompatActivity implements RaveDialog.OnRave
         RaveDialogConfig.initialise(false);
 
         setContentView(R.layout.activity_main);
-        raveButton = (Button) findViewById(R.id.rave_btn);
+        raveButtonPin = (Button) findViewById(R.id.rave_btn_auth_pin);
+        raveButtonVbp = (Button) findViewById(R.id.rave_btn_vbv_secure);
+        raveButtonNoAuth = (Button) findViewById(R.id.rave_btn_no_auth);
+        raveButtonRandom = (Button) findViewById(R.id.rave_btn_random_debit);
+
+        RaveDialogConfig.initialise(false);
 
 //         vbvssecure
 //
-//        raveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                RaveData raveData = new RaveData.Builder(
-//                        "Shawarma and Coke",
-//                        "Shawarma and Coke for kenny",
-//                        1400.00,
-//                        "FLWPUBK-b06794b2e72d2cf13215b220b04f314b-X",
-//                        "FLWSECK-d4c03911a1c2b2db5fbd39d8ca8c2825-X",
-//                        "kehinde.a.shittu@gmail.com",
-//                        "rave-dash-1483447695",
-//                        RaveAuthModel.VBV_SECURECODE)
-//                        .build();
-//                RaveDialog rave = new RaveDialog(MainActivity.this, raveData);
-//                rave.show();
-//            }
-//        });
+        raveButtonVbp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RaveData raveData = new RaveData.Builder(
+                        "Shawarma and Coke",
+                        "Shawarma and Coke for kenny",
+                        1400.00,
+                        "FLWPUBK-31e8501f7a46845633693ddcb42b25f8-X",//"FLWPUBK-6c011cdb12f0c81e332424b061047e7b-X",
+                        "FLWSECK-0ad3beb5bf9f7708c8e4bab43bd4af0d-X",//"FLWSECK-9796e060383fecbe90f984dfa9e2e1df-X",
+                        "jokafor@pacentltd.com",
+                        "rave-checkout-1489769562",//"rave-checkout-1485623235",
+                        RaveAuthModel.VBV_SECURECODE)
+                        .build();
+                RaveDialog rave = new RaveDialog(MainActivity.this, raveData, MainActivity.this);
+                rave.show();
+            }
+        });
 
         //no auth
 
-//        raveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                RaveData raveData = new RaveData.Builder(
-//                        "Shawarma and Coke",
-//                        "Shawarma and Coke for kenny",
-//                        1400.00,
-//                        "FLWPUBK-bcdc25e5a478ee7e465050b8d0c843f2-X",
-//                        "FLWSECK-a4e9c2480ddf7565a62c1a02c9be3252-X",
-//                        "kehinde.a.shittu@gmail.com",
-//                        "rave-checkout-1485605746",
-//                        RaveAuthModel.NOAUTH)
-//                        .build();
-//                RaveDialog rave = new RaveDialog(MainActivity.this, raveData);
-//                rave.show();
-//            }
-//        });
+        raveButtonNoAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RaveData raveData = new RaveData.Builder(
+                        "Shawarma and Coke",
+                        "Shawarma and Coke for kenny",
+                        1400.00,
+                        "FLWPUBK-31e8501f7a46845633693ddcb42b25f8-X",//"FLWPUBK-6c011cdb12f0c81e332424b061047e7b-X",
+                        "FLWSECK-0ad3beb5bf9f7708c8e4bab43bd4af0d-X",//"FLWSECK-9796e060383fecbe90f984dfa9e2e1df-X",
+                        "jokafor@pacentltd.com",
+                        "rave-checkout-1489769562",//"rave-checkout-1485623235",
+                        RaveAuthModel.NOAUTH)
+                        .build();
+                RaveDialog rave = new RaveDialog(MainActivity.this, raveData, MainActivity.this);
+                rave.show();
+            }
+        });
 
 //        // random debit
 //
-//        raveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                RaveData raveData = new RaveData.Builder(
-//                        "Shawarma and Coke",
-//                        "Shawarma and Coke for kenny",
-//                        10000.0,
-//                        "FLWPUBK-e8fdf9cf4d34c2084b040b5f8b214844-X",
-//                        "FLWSECK-fc2285159317c22ffaf3c811a68bc430-X",
-//                        "kehinde.a.shittu@gmail.com",
-//                        "rave-checkout-1485623235",
-//                        RaveAuthModel.RANDOM_DEBIT)
-//                        .build();
-//                RaveDialog rave = new RaveDialog(MainActivity.this, raveData);
-//                rave.show();
-//            }
-//        });
+        raveButtonRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RaveData raveData = new RaveData.Builder(
+                        "Shawarma and Coke",
+                        "Shawarma and Coke for kenny",
+                        10000.0,
+                        "FLWPUBK-31e8501f7a46845633693ddcb42b25f8-X",//"FLWPUBK-6c011cdb12f0c81e332424b061047e7b-X",
+                        "FLWSECK-0ad3beb5bf9f7708c8e4bab43bd4af0d-X",//"FLWSECK-9796e060383fecbe90f984dfa9e2e1df-X",
+                        "jokafor@pacentltd.com",
+                        "rave-checkout-1489769562",//"rave-checkout-1485623235",
+                        RaveAuthModel.RANDOM_DEBIT)
+                        .build();
+                RaveDialog rave = new RaveDialog(MainActivity.this, raveData,MainActivity.this);
+                rave.show();
+            }
+        });
 
 
 // pin
-        raveButton.setOnClickListener(new View.OnClickListener() {
+        raveButtonPin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RaveData raveData = new RaveData.Builder(
@@ -103,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements RaveDialog.OnRave
                         "rave-checkout-1489769562",//"rave-checkout-1485623235",
                         RaveAuthModel.PIN)
                         .build();
-                RaveDialog rave = new RaveDialog(MainActivity.this, raveData);
+                RaveDialog rave = new RaveDialog(MainActivity.this, raveData, MainActivity.this);
                 rave.show();
             }
         });
@@ -114,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements RaveDialog.OnRave
     @Override
     public void onResponse(Map<String, Object> data) {
         for (Map.Entry<String, Object> entry : data.entrySet()) {
-            Log.i("POT", "Entry Key :" + entry.getKey() + " Value: " + entry.getValue());
+            Log.i("POT", "Entry>>  Key :" + entry.getKey() + " || Value: " + entry.getValue());
         }
     }
 }
--- rave/src/main/java/com/flutterwave/rave/RaveDialogConfig.java
+++ rave/src/main/java/com/flutterwave/rave/RaveDialogConfig.java
@@ -6,13 +6,19 @@ package com.flutterwave.rave;
 
 public class RaveDialogConfig {
     public static String BASE_URL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api";
+    private static boolean isProduction = false;
 
-    public static void initialise(boolean isProduction) {
+    public static void initialise(boolean isProd) {
+        isProduction = isProd;
         if (isProduction) {
             BASE_URL = "https://api.ravepay.co/flwv3-pug/getpaidx/api";
         } else {
             BASE_URL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api";
         }
 
+    }
+
+    public static boolean isProduction() {
+        return isProduction;
     }
 }
